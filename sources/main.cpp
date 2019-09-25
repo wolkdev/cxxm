6927f3d05423039faa073aa9a7545aa9f6158af0
Corentin <corentin2kr@gmail.com>
2019-09-24 22:49:41 -0400

    GetCMakeFile function

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,43 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argc, char const *argv[])
+#include <filesystem>
+
+std::filesystem::path GetCMakeFile()
 {
+    std::filesystem::path path = std::filesystem::current_path() / "start";
+
+    do
+    {
+        path = path.parent_path();
+
+        const std::filesystem::path& cmakepath = path / "CMakeList.txt";
+
+        if (std::filesystem::exists(cmakepath))
+        {
+            return cmakepath;
+        }
+    }
+    while (path.has_filename());
+
+    return path;
+}
+
+int main(int _argc, char const* _argv[])
+{
+    const std::filesystem::path& cmakepath = GetCMakeFile();
+
+    if (cmakepath.has_filename())
+    {
+        std::cout << cmakepath << std::endl;
+    }
+    
+    if (_argc > 1)
+    {
+        std::string cmd = _argv[0];
+    }
+
     return 0;
 }
