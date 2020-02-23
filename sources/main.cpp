8ceeb83658e51e1f0a2fc0aef2b61f2528881acb
Corentin <corentin2kr@gmail.com>
2020-02-22 20:44:52 -0400

    cmd update

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,7 +7,8 @@ int main(int _argc, char const* _argv[])
     // - command line completion
     // - use ctest for tests in cmakelists
     // - change include on move / remove in all project files
-    // - upgrade cmd (no need commander, tell if cmd doesn't exists, ...)
+    // - command helps
+    // - init create dir with proj name
 
     if (_argc > 1)
     {
@@ -15,15 +16,7 @@ int main(int _argc, char const* _argv[])
         int argc = _argc - 2;
         const char** argv = (argc > 0 ? &_argv[2] : nullptr);
 
-        cmd commander;
-
-        commander.add(init_cmd);
-        commander.add(add_cmd);
-        commander.add(move_cmd);
-        commander.add(remove_cmd);
-        commander.add(dump_cmd);
-        
-        commander.execute(command, argc, argv);
+        cmd::execute(command, argc, argv);
     }
 
     std::cout << std::endl;
